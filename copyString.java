07baffb4b23545625fe5677b02eac66ba40312a2
Akash Gupta <36152448+Akashgupta333@users.noreply.github.com>
2019-08-09 22:03:45 +0530

    Copy one String to another

---
+++ copyString.java
@@ -0,0 +1,12 @@
+
+public class copyString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String s1 = "Akash";
+        String s2 = "";
+        s2 = s2.concat(s1);
+        System.out.println(s2);
+	}
+
+}

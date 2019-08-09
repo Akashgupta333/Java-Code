b75f15ac18e71210a1a852bbd984529107e82cb8
Akash Gupta <36152448+Akashgupta333@users.noreply.github.com>
2019-08-09 21:39:29 +0530

    Left rotate of array.

---
+++ LeftRotate.java
@@ -0,0 +1,18 @@
+package Demo;
+
+public class LeftRotate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int a[]={1,2,3,4,5,6};
+   int n=2;
+   for(int i=0;i<n;i++){
+	   int j,first;
+	   first=a[0];
+   for(j=0;j<a.length-1;j++){
+	   a[j]=a[j+1];
+   }
+       
+	}
+	}
+}

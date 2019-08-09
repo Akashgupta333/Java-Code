4e4de4efbcb35bf5caf815520500f68f3bebd030
Akash Gupta <36152448+Akashgupta333@users.noreply.github.com>
2019-08-09 21:49:11 +0530

    Print Vowels & Consonants in String.

---
+++ vowelsAndConsonents.java
@@ -0,0 +1,24 @@
+
+public class vowelsAndConsonents {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String str = "This is a really simple sentence";
+        int vCount = 0,cCount = 0;
+        str = str.toLowerCase();
+        for(int i=0;i<str.length();i++){
+        	if(str.charAt(i) == 'a' ||
+        		str.charAt(i) == 'e' ||
+        		str.charAt(i) == 'i' ||
+        		str.charAt(i) == 'o' ||
+        		str.charAt(i) == 'u'){
+        		vCount++;
+        	}
+        	else  if(str.charAt(i) >= 'a' && str.charAt(i)<='z'){
+        		cCount++;
+        	}
+        }
+        System.out.println(vCount+"  "+cCount);
+	}
+
+}

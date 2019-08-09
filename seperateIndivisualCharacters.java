5d6197d2a602c60eeaeb79b7ba727f062d927956
Akash Gupta <36152448+Akashgupta333@users.noreply.github.com>
2019-08-09 21:54:51 +0530

    Print separate individual character from String.

---
+++ seperateIndivisualCharacters.java
@@ -0,0 +1,12 @@
+
+public class seperateIndivisualCharacters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String str = "AkashGupta";
+        for(int i=0;i<str.length();i++){
+        	System.out.print(str.charAt(i)+" ");
+        }
+	}
+
+}

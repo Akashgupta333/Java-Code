5603ba168d0d9824c4fa48f9b01e1b52d1cecb89
Akash Gupta <36152448+Akashgupta333@users.noreply.github.com>
2019-08-13 12:21:48 +0530

    Java Pattern program
    EDCBA
    EDCBA
    EDCBA
    EDCBA
    EDCBA

---
+++ Pattern9.java
@@ -0,0 +1,15 @@
+
+public class Pattern9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(char i='E';i>='A';i--){
+			for(char j='E';j>='A';j--){
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	
+	}
+
+}

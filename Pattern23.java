32baeb3ecf5043f66251cee1e25d0479141e3d42
Akash Gupta <36152448+Akashgupta333@users.noreply.github.com>
2019-08-20 14:57:06 +0530

    Java pattern program
    EDCBA
    EDCB
    EDC
    ED
    E

---
+++ Pattern23.java
@@ -0,0 +1,14 @@
+
+public class Pattern23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(char i='A';i<='E';i++){
+			for(char j='E';j>=i;j--){
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+
+}

700eaaccccbb6d7a392a7df3e596e4b846404336
Akash Gupta <36152448+Akashgupta333@users.noreply.github.com>
2019-08-20 14:56:10 +0530

    Java pattern program
    EEEEE
    DDDD
    CCC
    BB
    A

---
+++ Pattern22.java
@@ -0,0 +1,14 @@
+
+public class Pattern22 {
+
+	public static void main(String[] args) {
+ 		// TODO Auto-generated method stub
+		for(char i='E';i>='A';i--){
+			for(char j='A';j<=i;j++){
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+
+}
